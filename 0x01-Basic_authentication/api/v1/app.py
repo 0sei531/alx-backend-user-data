646c570ea19757242c7b3c792586cc61bdd091d3
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-07 10:45:26 -1200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,34 +21,32 @@ elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-@app.errorhandler(404)
-def not_found_error(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ Unauthorized handler
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """ Forbidden handler
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler """
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.before_request
-def before_request() -> None:
-    """ Execute before each request
-    """
+def before_request() -> str:
+    """ Before Request Handler """
     if auth is None:
         return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -59,7 +57,8 @@ def before_request() -> None:
     if auth.current_user(request) is None:
         abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
