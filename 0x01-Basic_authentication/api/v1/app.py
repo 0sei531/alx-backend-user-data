c49079066e9542e5f151974c847f81119ebdaf11
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-07 11:22:43 -1200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,29 +24,33 @@ elif AUTH_TYPE == "basic_auth":
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ Unauthorized handler """
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """ Forbidden handler """
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
 def before_request() -> str:
-    """ Before Request Handler """
+    """Before Request Handler"""
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
