ed7e462ff775dd34ec992ab09be49cae9b1c99c7
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-07 11:21:19 -1200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def before_request() -> str:
     """ Before Request Handler """
     if auth is None:
         return
-    
+
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if not auth.require_auth(request.path, excluded_paths):
