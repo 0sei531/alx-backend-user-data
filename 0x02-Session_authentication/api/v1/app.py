b7476ada561b1d9daf60699a065130cfa4f3fed0
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-09 11:24:27 -1200

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,10 +2,12 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 
@@ -13,6 +15,7 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 # Authentication Setup
 auth = None
@@ -33,6 +36,7 @@ elif AUTH_TYPE == "session_exp_auth":
 elif AUTH_TYPE == "session_db_auth":
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
+
 
 @app.before_request
 def before_request() -> None:
@@ -55,20 +59,24 @@ def before_request() -> None:
             abort(403)
         request.current_user = auth.current_user(request)
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Handle 404 errors """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """ Handle 401 errors """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
     """ Handle 403 errors """
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
