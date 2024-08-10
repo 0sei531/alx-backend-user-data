4379f15663ec16ac18ec88a6fa9c4e776a0c8fdf
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-09 12:16:15 -1200

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,13 +19,11 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-
     def user_id_for_session_id(self, session_id: str = None) -> str:
         ''' Return user ID associated with specified session ID. '''
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
-
 
     def current_user(self, request=None):
         ''' Returns a User instance based on a cookie value '''
@@ -34,7 +32,6 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
-
 
     def destroy_session(self, request=None):
         """Deletes the user session / logout"""
