66c49324365710fd41f72c4b2e33088ffdeaef30
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-09 12:29:07 -1200

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,7 +7,11 @@ from models.user import User
 from os import getenv
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route(
+    '/auth_session/login',
+    methods=['POST'],
+    strict_slashes=False
+)
 def login():
     """ POST /auth_session/login
     Return:
@@ -41,7 +45,11 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE'],
+    strict_slashes=False
+)
 def logout():
     """ DELETE /auth_session/logout
     Return:
