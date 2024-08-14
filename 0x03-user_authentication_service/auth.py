eb5e1e98a90a3410fe50bd9c0e736e6e34597e6b
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-14 09:29:46 -1200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,16 +46,12 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            password_bytes = password.encode('utf-8')
-            hashed_password = user.hashed_password
-            return bcrypt.checkpw(password_bytes, hashed_password)
-            except NoResultFound:
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
             return False
 
-
     def create_session(self, email: str) -> Union[str, None]:
-        """ Creates a new session ID for the user
-        and stores it in the database.
+        """ Creates a new session ID for the user and stores it in the database.
 
         Returns the session ID as a string, or None if the user is not found.
         """
@@ -89,9 +85,7 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """ Generates a reset password token and updates the user's record.
 
-        Returns the reset token, or raises a ValueError
-        if the user is not found.
-
+        Returns the reset token, or raises a ValueError if the user is not found.
         """
         try:
             user = self._db.find_user_by(email=email)
@@ -104,8 +98,7 @@ class Auth:
     def update_password(self, reset_token: str, password: str) -> None:
         """ Updates the user's password using the provided reset token.
 
-        Resets the password and invalidates the reset token. Raises
-        a ValueError if the token is invalid.
+        Resets the password and invalidates the reset token. Raises a ValueError if the token is invalid.
         """
         if reset_token is None or password is None:
             raise ValueError("Reset token and password cannot be None")
