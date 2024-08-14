6535f5e4ca2c787b4bd94e4597a1b3372e43ee18
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-14 09:07:40 -1200

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,101 +1,65 @@
 #!/usr/bin/env python3
-"""
-Database module for ORM with SQLAlchemy.
-
-This module provides a `DB` class to manage a SQLite database for a `User` model.
-It includes methods to add, find, and update users in the database.
-"""
-
+""" DB class """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
 class DB:
-    """Database class for Object-Relational Mapping (ORM) with SQLAlchemy."""
-
+    """ DB class """
     def __init__(self):
-        """Initializes the database engine and session."""
+        """ constructor """
         self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)  # Reset the database for fresh start
-        Base.metadata.create_all(self._engine)  # Create tables
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Creates and returns a database session if one does not exist."""
+        """ session getter """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Adds a new user to the database.
-
-        Args:
-            email (str): The user's email address.
-            hashed_password (str): The hashed password for the user.
-
-        Returns:
-            User: The newly created User object.
-        """
+        """ adds and returns a new user Object """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        self._session.add(user)  # add the new user
+        self._session.commit()  # update the DataBase
+
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Finds a user in the database by arbitrary keyword arguments.
+        """ function that finds a created user
+        using a argument """
+        if kwargs is None:
+            raise InvalidRequestError
 
-        Args:
-            **kwargs: Arbitrary keyword arguments corresponding to User attributes.
+        for i in kwargs.keys():
+            if i not in User.__table__.columns.keys():
+                raise InvalidRequestError
 
-        Returns:
-            User: The first User object that matches the filters.
+        user_query = self._session.query(User).filter_by(
+            **kwargs).first()
 
-        Raises:
-            InvalidRequestError: If no keyword arguments are provided or invalid keys are used.
-            NoResultFound: If no user matches the filters.
-        """
-        if not kwargs:
-            raise InvalidRequestError("No keyword arguments provided.")
+        if user_query is None:
+            raise NoResultFound
 
-        valid_keys = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in valid_keys:
-                raise InvalidRequestError(f"Invalid key '{key}' in kwargs.")
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound("No user found matching the criteria.")
-
-        return user
+        return user_query
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """
-        Updates a user's attributes in the database.
-
-        Args:
-            user_id (int): The ID of the user to update.
-            **kwargs: Arbitrary keyword arguments corresponding to User attributes to update.
-
-        Raises:
-            ValueError: If an invalid key is provided in kwargs.
-            NoResultFound: If no user is found with the given user_id.
-        """
+        """ update properties of an user """
         user = self.find_user_by(id=user_id)
+        for i in kwargs.keys():
+            if i not in User.__table__.columns.keys():
+                raise ValueError
 
-        valid_keys = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in valid_keys:
-                raise ValueError(f"Invalid key '{key}' in kwargs.")
-
-        for key, value in kwargs.items():
-            setattr(user, key, value)
+        for k, v in kwargs.items():
+            setattr(user, k, v)
 
         self._session.commit()
